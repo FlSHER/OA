758b3d49d27bb72f53cc84a60407d9c94e41f1ac
Fisher <544473461@qq.com>
2017-07-21 17:41:56 +0800

    Merge origin/master

--- database/migrations/2017_07_21_091525_create_staff_manage_tables.php
+++ database/migrations/2017_07_21_091525_create_staff_manage_tables.php
@@ -45,6 +45,7 @@ class CreateStaffManageTables extends Migration {
         Schema::create('staff_info', function (Blueprint $table) {
             $table->mediumInteger('staff_sn')->unsigned()->primary();
             $table->char('account_number', 19)->comment('银行卡号');
+            $table->char('account_bank', 20)->comment('开户行');
             $table->timestamps();
             $table->softDeletes();
         });
