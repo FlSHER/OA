1f6ec9fd4a5098e7e7e7d4fe450db282615e40de
刘勇 <1032380143@qq.com>
2017-09-04 10:44:07 +0800

    ROUTE

--- app/Models/Reimburse/Reim_department.php
+++ app/Models/Reimburse/Reim_department.php
@@ -3,9 +3,11 @@
 namespace App\Models\Reimburse;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reim_department extends Model {
 
+    use SoftDeletes;
     public $timestamps = false;
     protected $connection = 'reimburse_mysql';
     protected $fillable = ['name'];
