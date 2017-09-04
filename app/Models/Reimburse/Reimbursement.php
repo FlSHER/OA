9a738a25a6de152e2cc58b24f5f1bcbe1c7c3dd0
刘勇 <1032380143@qq.com>
2017-09-04 10:44:17 +0800

    ROUTE

--- app/Models/Reimburse/Reimbursement.php
+++ app/Models/Reimburse/Reimbursement.php
@@ -15,7 +15,7 @@ class Reimbursement extends Model
      * 资金归属
      */
     public function reim_department() {
-        return $this->belongsTo('App\Models\Reimburse\Reim_department');
+        return $this->belongsTo('App\Models\Reimburse\Reim_department')->withTrashed();
     }
 
     public function expenses() {//消费明细
