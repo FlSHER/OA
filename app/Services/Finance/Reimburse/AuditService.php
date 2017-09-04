b1a6ab211550bb67cd9be31cf747752a92207781
刘勇 <1032380143@qq.com>
2017-09-04 11:24:54 +0800

    ROUTE

--- app/Services/Finance/Reimburse/AuditService.php
+++ app/Services/Finance/Reimburse/AuditService.php
@@ -243,6 +243,7 @@ class AuditService
         }
         $expense_where = ['expenses' => function ($query) {
             $query->where('is_audited', '=', 1);
+            $query->orderBy('date','asc');
         }];
         $result = app('Plugin')->dataTables($request, Reimbursement::with('expenses.type')->with($expense_where), $where);
         return $result['data'];
