20d54939cf26d2ccb4843e17b44d377b5cb5964e
admin <1032380143@qq.com>
2017-09-26 15:55:29 +0800

    ROUTE

--- app/Http/Controllers/Api/AppraiseController.php
+++ app/Http/Controllers/Api/AppraiseController.php
@@ -15,18 +15,17 @@ class AppraiseController extends Controller
      */
     public function appraiseFromSubmit(Request $request)
     {
-        return 1111;
         $validator = Validator::make($request->all(), [
             'staff_sn' => 'required|exists:staff,staff_sn',
             'remark' => 'required|string',
         ], [], trans('fields.appraise'));
         if ($validator->fails()) {
-            return ['msg' => 'error', 'result' => $validator->errors()];
+            return app('ApiResponse')->makeErrorResponse(['msg' => 'error', 'result' => $validator->errors()], 500);
         }
         $data = $request->except(['_url']);
         $data['entry_staff_sn'] = app('CurrentUser')->staff_sn;
         $data['entry_name'] = app('CurrentUser')->realname;
         Appraise::insert($data);
-        return ['msg'=>'success'];
+        return app('ApiResponse')->makeSuccessResponse(['msg'=>'success'], 200);
     }
 }
