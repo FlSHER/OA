fee1901c3ccb70b3c582a6a9bd493999711e1fd6
admin <1032380143@qq.com>
2017-09-27 15:16:58 +0800

    ROUTE

--- app/Models/HR/Appraise.php
+++ app/Models/HR/Appraise.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Appraise extends Model
 {
     protected  $table='staff_appraise';
-    protected  $fillable =['staff_sn','remark','entry_staff_sn','entry_name'];
+    protected  $fillable =['staff_sn','position','department','shop','remark','entry_staff_sn','entry_name'];
 }
