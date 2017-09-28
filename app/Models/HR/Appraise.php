be95decf54b04dc28b4e34558e958cd2cb564900
admin <1032380143@qq.com>
2017-09-28 16:26:16 +0800

    ROUTE

--- app/Models/HR/Appraise.php
+++ app/Models/HR/Appraise.php
@@ -10,6 +10,6 @@ class Appraise extends Model
     protected  $fillable =['staff_sn','position','department','shop','remark','entry_staff_sn','entry_name'];
 
     public function staff(){
-        return $this->hasOne('App\Models\HR\Staff','staff_sn');
+        return $this->hasOne('App\Models\HR\Staff','staff_sn','staff_sn');
     }
 }
