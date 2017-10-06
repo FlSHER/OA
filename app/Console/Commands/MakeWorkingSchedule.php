24b11a319210deb46fd10a8f6394045d00d345fb
Fisher <544473461@qq.com>
2017-10-06 18:01:53 +0800

    生成店铺排班表的定时任务中加入日志节点，测试是否正常运行

--- app/Console/Commands/MakeWorkingSchedule.php
+++ app/Console/Commands/MakeWorkingSchedule.php
@@ -39,20 +39,23 @@ class MakeWorkingSchedule extends Command
      */
     public function handle()
     {
+        Log::info('Start making working schedule');
         $tableYesterday = DB::connection('attendance')->select('SELECT * FROM information_schema.TABLES WHERE table_name ="working_schedule_' . date('Ymd', strtotime("-1 day")) . '"');
         $tableToday = DB::connection('attendance')->select('SELECT * FROM information_schema.TABLES WHERE table_name ="working_schedule_' . date('Ymd') . '"');
         if (empty($tableToday) && !empty($tableYesterday)) {
             DB::connection('attendance')->statement('CREATE TABLE working_schedule_' . date('Ymd') . ' LIKE working_schedule_' . date('Ymd', strtotime("-1 day")));
             DB::connection('attendance')->statement('INSERT INTO working_schedule_' . date('Ymd') . ' SELECT * FROM working_schedule_' . date('Ymd', strtotime("-1 day")));
         } elseif (empty($tableToday)) {
-            Log::info('Table working_schedule dosen\'t exists');
+            Log::error('Table working_schedule dosen\'t exists');
             $this->info('Table working_schedule dosen\'t exists');
             return false;
         }
+        $addCount = 0;
+        $deleteCount = 0;
         DB::table('staff')
             ->select(DB::raw('GROUP_CONCAT(staff_sn) AS staff_sn'), 'shop_sn')
             ->where('shop_sn', '<>', '')->groupBy('shop_sn')
-            ->get()->each(function ($model) {
+            ->get()->each(function ($model) use ($addCount, $deleteCount) {
                 $BasicStaffSnList = explode(',', $model->staff_sn);
                 $ScheduleStaffSnList = DB::connection('attendance')
                     ->table('working_schedule_' . date('Ymd'))
@@ -61,17 +64,21 @@ class MakeWorkingSchedule extends Command
                 $addList = empty($ScheduleStaffSnList) ? $BasicStaffSnList : array_diff($BasicStaffSnList, $ScheduleStaffSnList);
                 $deleteList = empty($ScheduleStaffSnList) ? [] : array_diff($ScheduleStaffSnList, $BasicStaffSnList);
                 foreach ($addList as $staffSn) {
+                    $addCount++;
                     DB::connection('attendance')
                         ->table('working_schedule_' . date('Ymd'))
                         ->insert(['shop_sn' => $model->shop_sn, 'staff_sn' => $staffSn]);
                 }
                 foreach ($deleteList as $staffSn) {
+                    $deleteCount++;
                     DB::connection('attendance')
                         ->table('working_schedule_' . date('Ymd'))
                         ->where(['shop_sn' => $model->shop_sn, 'staff_sn' => $staffSn])
                         ->delete();
                 }
             });
+        Log::info('add:' . $addCount . ' delete:' . $deleteCount);
         DB::connection('attendance')->select('DROP TABLE IF EXISTS working_schedule_' . date('Ymd', strtotime("-7 day")));
+        Log::info('End making working schedule');
     }
 }
