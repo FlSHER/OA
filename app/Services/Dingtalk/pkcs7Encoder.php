87f118a8adc4113b8ed9fe5b8a1b148e738b72dc
Fisher <544473461@qq.com>
2018-05-06 16:30:47 +0800

    钉钉加密兼容php7.2

--- app/Services/Dingtalk/pkcs7Encoder.php
+++ app/Services/Dingtalk/pkcs7Encoder.php
@@ -7,125 +7,108 @@ include_once "errorCode.php";
 
 class PKCS7Encoder
 {
-	public static $block_size = 32;
+    public static $block_size = 32;
 
-	function encode($text)
-	{
-		$block_size = PKCS7Encoder::$block_size;
-		$text_length = strlen($text);
-		$amount_to_pad = PKCS7Encoder::$block_size - ($text_length % PKCS7Encoder::$block_size);
-		if ($amount_to_pad == 0) {
-			$amount_to_pad = PKCS7Encoder::block_size;
-		}
-		$pad_chr = chr($amount_to_pad);
-		$tmp = "";
-		for ($index = 0; $index < $amount_to_pad; $index++) {
-			$tmp .= $pad_chr;
-		}
-		return $text . $tmp;
-	}
+    function encode($text)
+    {
+        $block_size = PKCS7Encoder::$block_size;
+        $text_length = strlen($text);
+        $amount_to_pad = PKCS7Encoder::$block_size - ($text_length % PKCS7Encoder::$block_size);
+        if ($amount_to_pad == 0) {
+            $amount_to_pad = PKCS7Encoder::block_size;
+        }
+        $pad_chr = chr($amount_to_pad);
+        $tmp = "";
+        for ($index = 0; $index < $amount_to_pad; $index++) {
+            $tmp .= $pad_chr;
+        }
+        return $text . $tmp;
+    }
 
-	function decode($text)
-	{
-		$pad = ord(substr($text, -1));
-		if ($pad < 1 || $pad > PKCS7Encoder::$block_size) {
-			$pad = 0;
-		}
-		return substr($text, 0, (strlen($text) - $pad));
-	}
+    function decode($text)
+    {
+        $pad = ord(substr($text, -1));
+        if ($pad < 1 || $pad > PKCS7Encoder::$block_size) {
+            $pad = 0;
+        }
+        return substr($text, 0, (strlen($text) - $pad));
+    }
 
 }
 
 
 class Prpcrypt
 {
-	public $key;
+    public $key;
 
-	function __construct($k)
-	{
-		$this->key = base64_decode($k . "=");
-	}
+    function __construct($k)
+    {
+        $this->key = base64_decode($k . "=");
+    }
 
-	public function encrypt($text, $corpid)
-	{
+    public function encrypt($text, $corpid)
+    {
 
-		try {
-			//获得16位随机字符串，填充到明文之前
-			$random = $this->getRandomStr();
-			$text = $random . pack("N", strlen($text)) . $text . $corpid;
-			// 网络字节序
-			$size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-			$module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-			$iv = substr($this->key, 0, 16);
-			//使用自定义的填充方式对明文进行补位填充
-			$pkc_encoder = new PKCS7Encoder;
-			$text = $pkc_encoder->encode($text);
-			mcrypt_generic_init($module, $this->key, $iv);
-			//加密
-			$encrypted = mcrypt_generic($module, $text);
-			mcrypt_generic_deinit($module);
-			mcrypt_module_close($module);
+        try {
+            $random = $this->getRandomStr();
+            $text = $random . pack("N", strlen($text)) . $text . $corpid;
+            $iv = substr($this->key, 0, 16);
+            $pkc_encoder = new PKCS7Encoder;
+            $text = $pkc_encoder->encode($text);
+            $encrypted = openssl_encrypt($text, 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING, $iv);
+            return array(ErrorCode::$OK, base64_encode($encrypted));
+        } catch (Exception $e) {
+            print $e;
+            return array(ErrorCode::$EncryptAESError, null);
+        }
+    }
 
-			//print(base64_encode($encrypted));
-			//使用BASE64对加密后的字符串进行编码
-			return array(ErrorCode::$OK, base64_encode($encrypted));
-		} catch (Exception $e) {
-			print $e;
-			return array(ErrorCode::$EncryptAESError, null);
-		}
-	}
+    public function decrypt($encrypted, $corpid)
+    {
 
-	public function decrypt($encrypted, $corpid)
-	{
-
-		try {
-			$ciphertext_dec = base64_decode($encrypted);
-			$module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-			$iv = substr($this->key, 0, 16);
-			mcrypt_generic_init($module, $this->key, $iv);
-
-			$decrypted = mdecrypt_generic($module, $ciphertext_dec);
-			mcrypt_generic_deinit($module);
-			mcrypt_module_close($module);
-		} catch (Exception $e) {
-			return array(ErrorCode::$DecryptAESError, null);
-		}
+        try {
+            $ciphertext_dec = base64_decode($encrypted);
+            $iv = substr($this->key, 0, 16);
+            $decrypted = openssl_decrypt($ciphertext_dec, 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING, $iv);
+        } catch (Exception $e) {
+            return array(ErrorCode::$DecryptAESError, null);
+        }
 
 
-		try {
-			//去除补位字符
-			$pkc_encoder = new PKCS7Encoder;
-			$result = $pkc_encoder->decode($decrypted);
-			//去除16位随机字符串,网络字节序和AppId
-			if (strlen($result) < 16)
-				return "";
-			$content = substr($result, 16, strlen($result));
-			$len_list = unpack("N", substr($content, 0, 4));
-			$xml_len = $len_list[1];
-			$xml_content = substr($content, 4, $xml_len);
-			$from_corpid = substr($content, $xml_len + 4);
-                        define("SUITE_KEY", $from_corpid);
-		} catch (Exception $e) {
-			print $e;
-			return array(ErrorCode::$DecryptAESError, null);
-		}
-		if ($from_corpid != $corpid)
-			return array(ErrorCode::$ValidateSuiteKeyError, null);
-		return array(0, $xml_content);
+        try {
+            //去除补位字符
+            $pkc_encoder = new PKCS7Encoder;
+            $result = $pkc_encoder->decode($decrypted);
+            //去除16位随机字符串,网络字节序和AppId
+            if (strlen($result) < 16)
+                return "";
+            $content = substr($result, 16, strlen($result));
+            $len_list = unpack("N", substr($content, 0, 4));
+            $xml_len = $len_list[1];
+            $xml_content = substr($content, 4, $xml_len);
+            $from_corpid = substr($content, $xml_len + 4);
+            define("SUITE_KEY", $from_corpid);
+        } catch (Exception $e) {
+            print $e;
+            return array(ErrorCode::$DecryptAESError, null);
+        }
+        if ($from_corpid != $corpid)
+            return array(ErrorCode::$ValidateSuiteKeyError, null);
+        return array(0, $xml_content);
 
-	}
+    }
 
-	function getRandomStr()
-	{
+    function getRandomStr()
+    {
 
-		$str = "";
-		$str_pol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-		$max = strlen($str_pol) - 1;
-		for ($i = 0; $i < 16; $i++) {
-			$str .= $str_pol[mt_rand(0, $max)];
-		}
-		return $str;
-	}
+        $str = "";
+        $str_pol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        $max = strlen($str_pol) - 1;
+        for ($i = 0; $i < 16; $i++) {
+            $str .= $str_pol[mt_rand(0, $max)];
+        }
+        return $str;
+    }
 
 }
 
