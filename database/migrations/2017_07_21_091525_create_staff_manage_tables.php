f6565deb0d74080e4524108df3a09e4ac8b6a9eb
Fisher <544473461@qq.com>
2018-08-06 09:52:11 +0800

    ref:根据数据库补全员工管理表迁移（未完成）

--- database/migrations/2017_07_21_091525_create_staff_manage_tables.php
+++ database/migrations/2017_07_21_091525_create_staff_manage_tables.php
@@ -25,21 +25,37 @@ class CreateStaffManageTables extends Migration
             $table->char('realname', 10)->comment('员工姓名');
             $table->char('mobile', 11)->comment('手机号码');
             $table->char('wechat_number', 20)->comment('微信号');
-            $table->char('dingding', 50)->comment('钉钉id');
+            $table->char('dingtalk_number', 50)->comment('钉钉id');
             $table->char('id_card_number', 18)->comment('身份证号');
-            $table->tinyInteger('gender_id')->unsigned()->comment('性别ID');
+            $table->unsignedTinyInteger('gender_id')->comment('性别ID');
             $table->date('birthday')->comment('生日');
-            $table->tinyInteger('brand_id')->unsigned()->comment('品牌ID');
-            $table->smallInteger('department_id')->unsigned()->comment('部门ID');
-            $table->smallInteger('position_id')->unsigned()->comment('职位ID');
+            $table->unsignedTinyInteger('brand_id')->comment('品牌ID');
+            $table->unsignedSmallInteger('department_id')->comment('部门ID');
+            $table->unsignedSmallInteger('position_id')->comment('职位ID');
             $table->char('shop_sn', 10)->comment('店铺编码');
             $table->tinyInteger('status_id')->comment('状态ID');
-            $table->tinyInteger('is_active')->unsigned()->comment('是否激活');
+            $table->unsignedTinyInteger('is_active')->comment('是否激活');
+            $table->unsignedTinyInteger('property_id')->comment('属性ID 0：无 1：108将 2：36天罡 3：24金刚 4：18罗汉');
             $table->date('hired_at')->comment('入职日期');
             $table->date('employed_at')->comment('转正日期');
             $table->date('left_at')->comment('离职日期');
+            $table->mediumInteger('household_province_id')->unsigned()->comment('户口所在地(省)');
+            $table->mediumInteger('household_city_id')->unsigned()->comment('户口所在地(市)');
+            $table->mediumInteger('household_county_id')->unsigned()->comment('户口所在地(区/县)');
+            $table->char('household_address', 30)->comment('户口所在地(详细地址)');
+            $table->mediumInteger('living_province_id')->unsigned()->comment('现居住地(省)');
+            $table->mediumInteger('living_city_id')->unsigned()->comment('现居住地(市)');
+            $table->mediumInteger('living_county_id')->unsigned()->comment('现居住地(区/县)');
+            $table->char('living_address', 30)->comment('现居住地(详细地址)');
             $table->timestamps();
+            $table->rememberToken();
             $table->softDeletes();
+            $table->foreign('brand')->references('id')->on('brands');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->foreign('shop_sn')->references('shop_sn')->on('shops');
+            $table->foreign('status_id')->references('id')->on('staff_status');
+            $table->foreign('property_id')->references('id')->on('staff_property');
         });
         /**
          * 员工详细信息表
@@ -58,14 +74,6 @@ class CreateStaffManageTables extends Migration
             $table->char('national', 10)->comment('民族');
             $table->char('marital_status', 10)->comment('婚姻状况');
             $table->char('politics', 10)->comment('政治面貌');
-            $table->mediumInteger('household_province_id')->unsigned()->comment('户口所在地(省)');
-            $table->mediumInteger('household_city_id')->unsigned()->comment('户口所在地(市)');
-            $table->mediumInteger('household_county_id')->unsigned()->comment('户口所在地(区/县)');
-            $table->char('household_address', 30)->comment('户口所在地(详细地址)');
-            $table->mediumInteger('living_province_id')->unsigned()->comment('现居住地(省)');
-            $table->mediumInteger('living_city_id')->unsigned()->comment('现居住地(市)');
-            $table->mediumInteger('living_county_id')->unsigned()->comment('现居住地(区/县)');
-            $table->char('living_address', 30)->comment('现居住地(详细地址)');
             $table->char('native_place', 30)->comment('籍贯');
             $table->char('education', 5)->comment('学历');
             $table->char('remark', 200)->comment('备注');
@@ -75,6 +83,25 @@ class CreateStaffManageTables extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        /**
+         * 员工状态
+         */
+        Schema::create('staff_status', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->char('name', 10)->comment('状态名称');
+            $table->unsignedTinyInteger('sort')->comment('排序');
+        });
+
+        /**
+         * 员工属性
+         */
+        Schema::create('staff_property', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->char('name', 10)->comment('属性名称');
+            $table->unsignedTinyInteger('sort')->comment('排序');
+        });
+
         /**
          * 关系人表
          */
@@ -108,6 +135,7 @@ class CreateStaffManageTables extends Migration
     {
         Schema::dropIfExists('staff');
         Schema::dropIfExists('staff_info');
+        Schema::dropIfExists('staff_status');
         Schema::dropIfExists('staff_relatives');
         Schema::dropIfExists('staff_relative_type');
     }
