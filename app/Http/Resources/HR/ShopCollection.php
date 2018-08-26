9bd38672f9d5d78c76a360e9ec4e6bf6b515ff92
Fisher <544473461@qq.com>
2018-08-26 15:52:24 +0800

    fix:店铺资源类抛出异常

--- app/Http/Resources/HR/ShopCollection.php
+++ app/Http/Resources/HR/ShopCollection.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ShopCollection extends ResourceCollection
 {
-    protected static $wrap = null;
+    public static $wrap = null;
 
     /**
      * Transform the resource collection into an array.
