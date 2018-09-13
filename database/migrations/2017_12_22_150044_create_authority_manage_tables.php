37e7a10896f4c8598348f7bfe89733b982ffe872
28youth <daviezhang@vip.qq.com>
2018-09-13 11:39:00 +0800

    chore：设置权限迁移表字段默认值

--- database/migrations/2017_12_22_150044_create_authority_manage_tables.php
+++ database/migrations/2017_12_22_150044_create_authority_manage_tables.php
@@ -17,13 +17,13 @@ class CreateAuthorityManageTables extends Migration
             $table->mediumIncrements('id');
             $table->char('auth_name', 10)->comment('权限名称');
             $table->char('access_url', 20)->comment('访问路由');
-            $table->mediumInteger('parent_id')->unsigned()->comment('上级权限ID');
+            $table->mediumInteger('parent_id')->unsigned()->default(0)->comment('上级权限ID');
             $table->tinyInteger('sort')->unsigned()->comment('排序');
             $table->tinyInteger('is_menu')->unsigned()->comment('是否展示为菜单');
             $table->char('menu_name', 10)->comment('菜单名称');
             $table->char('menu_logo', 50)->comment('菜单图标');
-            $table->tinyInteger('is_active')->unsigned()->comment('是否激活');
-            $table->tinyInteger('is_public')->unsigned()->comment('是否对所有人开放');
+            $table->tinyInteger('is_active')->unsigned()->default(0)->comment('是否激活');
+            $table->tinyInteger('is_public')->unsigned()->default(0)->comment('是否对所有人开放');
             $table->smallInteger('app_id')->unsigned()->comment('应用ID');
             $table->timestamps();
             $table->softDeletes();
