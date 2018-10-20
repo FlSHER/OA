77fef4baaa5f38e2b11b53fb9888c26d21a01720
1032380143 <1032380143@qq.com>
2018-10-20 11:16:17 +0800

    添加钉钉工作通知接口

--- database/migrations/2018_08_14_113309_create_dingtalk_messages_table.php
+++ database/migrations/2018_08_14_113309_create_dingtalk_messages_table.php
@@ -19,14 +19,11 @@ class CreateDingtalkMessagesTable extends Migration
             $table->unsignedInteger('agent_id')->comment('钉钉应用ID');
             $table->unsignedInteger('create_staff')->comment('创建人工号');
             $table->char('create_realname',20)->comment('创建人');
-            $table->text('userid_list')->nullable()->comment('钉钉接收者员工ID');
-            $table->text('dept_id_list')->nullable()->comment('钉钉接收者部门ID');
-            $table->unsignedTinyInteger('to_all_user')->default(0)->comment('发给钉钉全部用户 1是，0否');
-            $table->unsignedTinyInteger('errcode')->commont('发送状态， 0 成功');
-            $table->char('task_id',100)->nullable()->comment('创建的发送任务id');
-            $table->char('request_id',100)->comment('返回request的ID');
             $table->char('msgtype',10)->comment('消息请求类型，text，image,file,link,markdown,oa,action_card');
-            $table->text('data')->comment('请求msg数据');
+            $table->text('data')->comment('请求data数据');
+            $table->unsignedTinyInteger('errcode')->nullable()->commont('发送状态， 0 成功');
+            $table->char('task_id',100)->nullable()->comment('创建的发送任务id');
+            $table->char('request_id',100)->nullable()->comment('返回request的ID');
             $table->index('client_id');
             $table->index('agent_id');
             $table->index('create_staff');
