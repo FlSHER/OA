7e210c2de65b96b6962c9d9615fc6caacdd59105
张卫 <daviezhang@vip.qq.com>
2018-11-16 17:14:24 +0800

    fix：保存店铺标签错误

--- app/Http/Controllers/Api/HR/ShopController.php
+++ app/Http/Controllers/Api/HR/ShopController.php
@@ -52,8 +52,7 @@ class ShopController extends Controller
                 Staff::whereIn('staff_sn', $staffSn)->update(['shop_sn' => $shop->shop_sn]);
             }
             if (!empty($data['tags'])) {
-                $tags = array_column($data['tags'], 'id');
-                $shop->tags()->sync($tags);
+                $shop->tags()->sync($data['tags']);
             }
         });
 
@@ -98,8 +97,7 @@ class ShopController extends Controller
                 Staff::whereIn('staff_sn', $staffSn)->update(['shop_sn' => $shop->shop_sn]);
             }
             if (!empty($data['tags'])) {
-                $tags = array_column($data['tags'], 'id');
-                $shop->tags()->sync($tags);
+                $shop->tags()->sync($data['tags']);
             }
         });
 
@@ -150,8 +148,8 @@ class ShopController extends Controller
             'status_id' => 'bail|required|exists:shop_status,id',
             'department_id' => 'bail|exists:departments,id',
             'brand_id' => 'bail|exists:brands,id',
-            'opening_at' => 'bail|required',
-            'end_at' => 'bail|required|after_or_equal:'.$request->opening_at,
+            'opening_at' => 'bail|date_format:Y-m-d',
+            'end_at' => 'bail|date_format:Y-m-d|after_or_equal:'.$request->opening_at,
             'province_id' => 'bail|required|exists:i_district,id',
             'city_id' => 'bail|required|exists:i_district,id',
             'county_id' => 'bail|required|exists:i_district,id',
