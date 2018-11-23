072e56cbda2a0a64e4a5918ddf09500eecc76d99
张卫 <daviezhang@vip.qq.com>
2018-11-23 11:22:48 +0800

    chore: 翻译预约变动字段

--- app/Http/Controllers/Api/HR/StaffTmpController.php
+++ app/Http/Controllers/Api/HR/StaffTmpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\HR;
 
 use App\Models\HR\Staff;
 use App\Models\HR\StaffTmp;
+use App\Models\HR\CostBrand;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -19,6 +20,11 @@ class StaffTmpController extends Controller
     public function index(Staff $staff)
     {
         $list = $staff->tmp()->with(['staff', 'admin'])->get();
+        $list->map(function ($item) {
+            $item->changes = $this->trans($item);
+
+            return $item;
+        });
 
         return response()->json($list, 200);
     }
@@ -59,5 +65,55 @@ class StaffTmpController extends Controller
         $tmp->load(['staff', 'admin']);
 
         return response()->json($tmp, 201);
+    }
+
+    /**
+     * 翻译预约变动字段.
+     * 
+     * @param  object $item
+     * @return array
+     */
+    public function trans($item): array
+    {
+        $trans = trans('fields.staff');
+        $relationMap = [
+            'shop_sn' => 'shop.name',
+            'brand_id' => 'brand.name',
+            'status_id' => 'status.name',
+            'position_id' => 'position.name',
+            'department_id' => 'department.name',
+        ];
+        $changes = [];
+        foreach ($item->changes as $key => $change) {
+            if (array_has($relationMap, $key)) {
+                $relationKey = $relationMap[$key];
+                $key = $trans[$relationKey];
+                $value = $this->getRelationName($relationKey, $change);
+            }
+            if (is_array($change)) {
+                $key = $trans[$key];
+                $value = CostBrand::whereIn('id', $change)->pluck('name');
+            }
+            $changes[$key] = $value;
+        }
+
+        return $changes;
+    }
+
+    /**
+     * 获取员工变动关系名称.
+     * 
+     * @param  string $relationKey
+     * @param  int $change
+     * 
+     * @return string
+     */
+    public function getRelationName($relationKey, $change)
+    {
+        $model = new Staff;
+        $relation = explode('.', $relationKey)[0];
+        $result = $model->$relation()->getModel()->find($change);
+
+        return $result->getAttribute('name');
     }
 }
