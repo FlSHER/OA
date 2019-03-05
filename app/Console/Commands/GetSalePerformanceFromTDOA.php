9431bce0bc2097e18ea809f157c10356bfedd509
Fisher <544473461@qq.com>
2019-03-05 10:13:49 +0800

    ref:重写利鲨考勤同步外汇表任务

--- app/Console/Commands/GetSalePerformanceFromTDOA.php
+++ app/Console/Commands/GetSalePerformanceFromTDOA.php
@@ -40,17 +40,17 @@ class GetSalePerformanceFromTDOA extends Command
     public function handle()
     {
         $startDate = date('Y-m-d', strtotime('-20 days'));
-        $data = DB::connection('TDOA')->table('flow_data_42 AS a')
-            ->join('flow_run AS b', 'a.run_id', '=', 'b.run_id')
+        $data = DB::connection('TDOA')->table('bpm_data_22 AS a')
+            ->join('bpm_run AS b', 'a.run_id', '=', 'b.run_id')
             ->select(
-                'a.data_7 as shop_sn',
-                'a.data_8 as attendance_date',
-                DB::raw('SUM(a.data_62+a.data_25+a.data_26+a.data_27) as sale_performance'),
+                'a.data_m924 as shop_sn',
+                'a.data_m925 as attendance_date',
+                DB::raw('SUM(a.data_m935+a.data_m936+a.data_m937+a.data_m938) as sale_performance'),
                 DB::raw('!ISNULL(MIN(b.END_TIME)) as is_end')
             )
-            ->groupBy('a.data_7', 'a.data_8')
+            ->groupBy('a.data_m924', 'a.data_m925')
             ->where('b.DEL_FLAG', '=', 0)
-            ->where(DB::raw('STR_TO_DATE(a.data_8,"%Y-%m-%d")'), '>', $startDate)
+            ->where(DB::raw('STR_TO_DATE(a.data_m925,"%Y-%m-%d")'), '>', $startDate)
             ->get()->toJson();
         DB::connection('attendance')->table('tdoa_sale_performance')->delete();
         DB::connection('attendance')->table('tdoa_sale_performance')->insert(json_decode($data, true));
